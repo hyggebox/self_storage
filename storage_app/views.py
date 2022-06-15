afe82fbaa391fcf001677dd0a6298ab652f9a994
Anastasia <asya_apto@mail.ru>
2022-06-15 16:52:59 +0300

    Add basic signup

--- storage_app/views.py
+++ storage_app/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
+
+
+from django.shortcuts import render
+
+from django.contrib.auth.forms import UserCreationForm
+
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import RegistrationForm
+
+
+# def signup(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             raw_password = form.cleaned_data.get('password1')
+#             user = authenticate(username=username, password=raw_password)
+#             login(request, user)
+#             return redirect('index')
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'registration/signup.html', {'form': form})
+
+
+class SignUp(CreateView):
+    form_class = RegistrationForm
+    success_url = reverse_lazy('my_rent')
+    template_name = 'registration/signup.html'
 
 
 def index(request):
