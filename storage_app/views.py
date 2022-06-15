fb9639f66811b1f3f62b75c33821d9f681d4e64c
Anastasia <asya_apto@mail.ru>
2022-06-16 00:36:57 +0300

    Sign up through modal window on main page

--- storage_app/views.py
+++ storage_app/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
+from django.contrib.auth.models import User
 from .forms import RegistrationForm
 from .models import Storage
 
@@ -18,20 +19,37 @@ class SignUp(CreateView):
 
 
 def index(request):
-    if request.method == "POST":
-        user = authenticate(username=request.POST["EMAIL"], password=request.POST["PASSWORD"])
-        print(user.client.phone)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return redirect(my_rent)
+    if request.method == 'POST':
+        if 'EMAIL' in request.POST:
+            user = authenticate(username=request.POST['EMAIL'],
+                                password=request.POST['PASSWORD'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect(my_rent)
+                else:
+                    # TODO - Return a 'disabled account' error message
+                    ...
             else:
-                # Return a 'disabled account' error message
+                print('No user')
+                # TODO - Return an 'invalid login' error message.
                 ...
+            return redirect(index)
+
+        elif 'EMAIL_CREATE' in request.POST and request.POST['PASSWORD_CREATE'] == request.POST['PASSWORD_CONFIRM']:
+
+            new_user = User.objects.filter(username=request.POST['EMAIL_CREATE']).first()
+            if not new_user:
+                User.objects.create_user(
+                    username=request.POST['EMAIL_CREATE'],
+                    password=request.POST['PASSWORD_CREATE']
+                )
+
         else:
-            # Return an 'invalid login' error message.
-            ...
-        return redirect(index)
+            user = User.objects.filter(username=request.POST['EMAIL_FORGET']).first()
+            if user:
+                # TODO - Восстановление пароля
+                print(f'>>>>> отправить новый пароль на {user.username}')
 
     random_storage = random.choice(Storage.objects.all())
     storage_box_count = random_storage.boxes.count()
