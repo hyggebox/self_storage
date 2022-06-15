b7f23733b4b9e94529092160c15849f7470a10b9
Anastasia <asya_apto@mail.ru>
2022-06-15 17:21:19 +0300

    Merge

--- storage_app/views.py
+++ storage_app/views.py
@@ -1,33 +1,13 @@
 import random
 
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
-
-
-from django.shortcuts import render
-
-from .models import Storage
-from django.contrib.auth.forms import UserCreationForm
-
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+
 from .forms import RegistrationForm
+from .models import Storage
 
-
-# def signup(request):
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             username = form.cleaned_data.get('username')
-#             raw_password = form.cleaned_data.get('password1')
-#             user = authenticate(username=username, password=raw_password)
-#             login(request, user)
-#             return redirect('index')
-#     else:
-#         form = UserCreationForm()
-#     return render(request, 'registration/signup.html', {'form': form})
 
 
 class SignUp(CreateView):
@@ -59,6 +39,4 @@ def faq(request):
 
 @login_required(login_url='/users/login/')
 def my_rent(request):
-    if True:
-        return render(request, 'my-rent.html')
-    return render(request, 'my-rent-empty.html')
+    return render(request, 'my-rent.html')
