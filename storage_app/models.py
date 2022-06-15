3d6c90a576d13980929aab69b642c7b225c53bd0
rimprog <riminprog@gmail.com>
2022-06-15 13:57:49 +0300

    Move models methods to managers

--- storage_app/models.py
+++ storage_app/models.py
@@ -6,10 +6,12 @@ from django.db import models
 
 from phonenumber_field.modelfields import PhoneNumberField
 
-from storage_app.managers import BoxOrderQuerySet
+from storage_app.managers import StorageQuerySet, BoxQuerySet, BoxOrderQuerySet
 
 
 class Storage(models.Model):
+    """Склад где расположены боксы."""
+
     address = models.TextField(
         'адрес',
         help_text='ул. Манчестерская, д. 7, кв. 1'
@@ -37,32 +39,19 @@ class Storage(models.Model):
         default=150
     )
 
-    def count_min_box_price(self):
-        min_box_price = self.boxes.aggregate(models.Min('month_rent_price'))['month_rent_price__min']
+    objects = StorageQuerySet.as_manager()
 
-        return min_box_price
-
-    def count_squares_meters(self):
-        squares_meters_count = self.boxes.aggregate(models.Sum('size'))['size__sum']
-
-        return squares_meters_count
-
-    def count_free_squares_meters(self):
-        rented_squares_meters_count = self.boxes.filter(is_rented=True).aggregate(models.Sum('size'))['size__sum']
-        free_squares_meters_count = self.count_squares_meters() - rented_squares_meters_count
-
-        return free_squares_meters_count
-
-    def count_free_boxes(self):
-        free_boxes_count = self.boxes.count() - self.boxes.filter(is_rented=True).count()
-
-        return free_boxes_count
+    class Meta:
+        verbose_name = 'склад'
+        verbose_name_plural = 'склады'
 
     def __str__(self):
         return f'{self.alias}'
 
 
 class Box(models.Model):
+    """Боксы доступные к аренде."""
+
     storage = models.ForeignKey(
         Storage,
         on_delete=models.CASCADE,
@@ -79,6 +68,8 @@ class Box(models.Model):
     )
     is_rented = models.BooleanField('Бокс арендован', blank=True, default=False)
 
+    objects = BoxQuerySet.as_manager()
+
     def count_month_rent_price(self):
         first_meter_price = self.storage.first_square_meter_price
         rest_meters_price = self.storage.rest_square_meters_price
@@ -92,12 +83,16 @@ class Box(models.Model):
 
         super().save(*args, **kwargs)
 
+    class Meta:
+        verbose_name = 'бокс'
+        verbose_name_plural = 'боксы'
+
     def __str__(self):
         return f'Бокс №{self.id}'
 
 
 class Client(models.Model):
-    """Зарегистрированный клиент сервиса арендовавший бокс."""
+    """Клиент сервиса."""
 
     user = models.OneToOneField(
         User,
@@ -117,6 +112,8 @@ class Client(models.Model):
 
 
 class BoxOrder(models.Model):
+    """Заказ на аренду бокса."""
+
     box = models.ForeignKey(
         Box,
         on_delete=models.PROTECT,
@@ -153,6 +150,10 @@ class BoxOrder(models.Model):
         self.box.save(*args, **kwargs)
 
         super().delete(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'заказ'
+        verbose_name_plural = 'заказы'
 
     def __str__(self):
         return f'{self.box} на {self.rent_term} месяцев'
