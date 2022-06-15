f15eb167a9ef7dd9c60324b601142bb9a618eef8
rimprog <riminprog@gmail.com>
2022-06-15 18:59:59 +0300

    Move models methods from managers to models

--- storage_app/models.py
+++ storage_app/models.py
@@ -46,6 +46,27 @@ class Storage(models.Model):
 
     objects = StorageQuerySet.as_manager()
 
+    def count_min_box_price(self):
+        min_box_price = self.boxes.aggregate(models.Min('month_rent_price'))['month_rent_price__min']
+
+        return min_box_price
+
+    def count_squares_meters(self):
+        squares_meters_count = self.boxes.aggregate(models.Sum('size'))['size__sum']
+
+        return squares_meters_count
+
+    def count_free_squares_meters(self):
+        rented_squares_meters_count = self.boxes.filter(is_rented=True).aggregate(models.Sum('size'))['size__sum']
+        free_squares_meters_count = self.count_squares_meters() - rented_squares_meters_count
+
+        return free_squares_meters_count
+
+    def count_free_boxes(self):
+        free_boxes_count = self.boxes.count() - self.boxes.filter(is_rented=True).count()
+
+        return free_boxes_count
+
     class Meta:
         verbose_name = 'склад'
         verbose_name_plural = 'склады'
