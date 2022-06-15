a13515288987c2c2ddf486e95e5d88dab1e65a65
Anastasia <asya_apto@mail.ru>
2022-06-15 23:14:05 +0300

    Login through modal window on main page

--- storage_app/views.py
+++ storage_app/views.py
@@ -1,6 +1,7 @@
 import random
 
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
@@ -17,6 +18,21 @@ class SignUp(CreateView):
 
 
 def index(request):
+    if request.method == "POST":
+        user = authenticate(username=request.POST["EMAIL"], password=request.POST["PASSWORD"])
+        print(user.client.phone)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect(my_rent)
+            else:
+                # Return a 'disabled account' error message
+                ...
+        else:
+            # Return an 'invalid login' error message.
+            ...
+        return redirect(index)
+
     random_storage = random.choice(Storage.objects.all())
     storage_box_count = random_storage.boxes.count()
     free_box_count = storage_box_count - random_storage.boxes.filter(is_rented=True).count()
@@ -40,6 +56,6 @@ def faq(request):
     return render(request, 'faq.html')
 
 
-@login_required(login_url='/users/login/')
+@login_required(login_url='/?login=1')
 def my_rent(request):
     return render(request, 'my-rent.html')
