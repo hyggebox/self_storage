e973669748f21290eb9631c3182a63314c432e28
clownkill <alehnovich90@yandex.ru>
2022-06-15 15:04:19 +0300

    add base template

--- storage_app/urls.py
+++ storage_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, boxes, faq, my_rent
 
 urlpatterns = [
     path('', index, name='index'),
+    path('boxes', boxes, name='boxes'),
+    path('faq', faq, name='faq'),
+    path('my-rent', my_rent, name='my-rent'),
 ]
