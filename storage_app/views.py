00f40ff333fcdbd34292e7b743b8de6c5f28f661
rimprog <riminprog@gmail.com>
2022-06-17 18:47:46 +0300

    Add map with storages to index.html

--- storage_app/views.py
+++ storage_app/views.py
@@ -16,6 +16,68 @@ from .forms import RegistrationForm
 from .models import Storage, Box, Client, Email
 
 
+import folium
+
+
+def prepare_storage_object_info_html(storage_object):
+    box_rent_button = '<p><b>Есть доступные к аренде боксы.</b></p>' if storage_object.count_free_boxes() > 0 else '<p><b>Все боксы заняты, выберите другой склад.</b></p>'
+    storage_object_info_html = """
+        <h3>{storage_name}:</h3>
+        <p>{address}</p>
+        <p>{phone_number}</p>
+        <p>Аренда боксов от <b>{min_box_price} руб.</b> в мес.</p>
+        <p>{free_squares_meters_count} из {squares_meters_count} м² склада свободно</p>
+        <p>{free_boxes_count} из {boxes_count} боксов свободно</p>
+        {box_rent_button}
+        """.format(
+            storage_name=storage_object.alias,
+            address=storage_object.address,
+            phone_number=storage_object.phone,
+            min_box_price=storage_object.count_min_box_price(),
+            squares_meters_count=storage_object.count_squares_meters(),
+            free_squares_meters_count=storage_object.count_free_squares_meters(),
+            boxes_count=storage_object.boxes.count(),
+            free_boxes_count=storage_object.count_free_boxes(),
+            box_rent_button=box_rent_button
+        )
+
+    return storage_object_info_html
+
+
+def add_storage(folium_map, lat, lon, name, storage_object_info_html, image_url):
+    icon = folium.features.CustomIcon(
+        image_url,
+        icon_size=(80, 80),
+    )
+
+    popup_html = storage_object_info_html.encode('ascii', errors='xmlcharrefreplace').decode('utf-8')
+
+    folium.Marker(
+        [lat, lon],
+        tooltip=name.encode('ascii', errors='xmlcharrefreplace').decode('utf-8'),
+        icon=icon,
+        popup=popup_html
+    ).add_to(folium_map)
+
+
+def create_map():
+    city_centre = [51.672, 39.1843] # Voronezh centre
+
+    folium_map = folium.Map(location=city_centre, zoom_start=12)
+
+    storages = Storage.objects.all()
+    for storage in storages:
+        add_storage(
+            folium_map,
+            storage.latitude,
+            storage.longitude,
+            storage.alias,
+            prepare_storage_object_info_html(storage),
+            'https://www.gruzchiki-kiev.net/wp-content/uploads/2021/02/skklad.png'
+        )
+
+    return folium_map
+
 
 class SignUp(CreateView):
     form_class = RegistrationForm
@@ -78,10 +140,12 @@ def index(request):
     random_storage = random.choice(Storage.objects.all())
     storage_box_count = random_storage.boxes.count()
     free_box_count = storage_box_count - random_storage.boxes.filter(is_rented=True).count()
+    folium_map = create_map()
     context = {
         'storage': random_storage,
         'box_count': storage_box_count,
-        'free_box': free_box_count
+        'free_box': free_box_count,
+        'map': folium_map._repr_html_(),
     }
 
     return render(request, 'index.html', context)
