c48d475baf6628b858c878a4aa4c1741aa7f5699
Anastasia <asya_apto@mail.ru>
2022-06-17 18:53:09 +0300

    Send qr code to user email

--- storage_app/views.py
+++ storage_app/views.py
@@ -1,4 +1,8 @@
+import os
 import random
+from functools import reduce
+from urllib.parse import unquote
+from email.mime.image import MIMEImage
 
 import phonenumbers
 
@@ -10,10 +14,14 @@ from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
+from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
 
 from django.contrib.auth.models import User
+
+from self_storage import settings
 from .forms import RegistrationForm
-from .models import Storage, Box, Client, Email
+from .models import Storage, Box, Client, Email, BoxOrder
 
 
 
@@ -41,7 +49,7 @@ def index(request):
                 login(request, user)
                 return redirect(my_rent)
 
-            print('No user')
+
             return redirect('/?login=1')
 
         elif 'signup_button' in request.POST and request.POST['PASSWORD_CREATE'] == request.POST['PASSWORD_CONFIRM']:
@@ -53,7 +61,6 @@ def index(request):
                         parsed_phonenumber,
                         phonenumbers.PhoneNumberFormat.E164
                     )
-                    print(formatted_phonenumber, 'is ok')
                     with transaction.atomic():
                         created_user = User.objects.create_user(
                             username=request.POST['EMAIL_CREATE'],
@@ -65,7 +72,7 @@ def index(request):
                             phone=formatted_phonenumber,
                         )
                         login(request, created_user)
-                        return redirect(my_rent)
+
             return redirect('/?login=1')
 
         elif 'reset_button' in request.POST:
@@ -114,17 +121,51 @@ def faq(request):
 def my_rent(request):
     if request.method == 'POST':
         current_user = request.user
-        if request.POST['PASSWORD_EDIT'] != 'new password':
-            current_user.set_password(request.POST['PASSWORD_EDIT'])
-            current_user.save()
-        if current_user.client.phone != request.POST['PHONE_EDIT']:
-            parsed_phonenumber = phonenumbers.parse(request.POST['PHONE_EDIT'], 'RU')
-            if phonenumbers.is_valid_number(parsed_phonenumber):
-                formatted_phonenumber = phonenumbers.format_number(
-                    parsed_phonenumber,
-                    phonenumbers.PhoneNumberFormat.E164
-                )
-            current_user.client.phone = formatted_phonenumber
-            current_user.client.save()
+        if 'open_box' in request.POST:
+            order = BoxOrder.objects.get(pk=request.POST['open_box'])
+
+            subject = f'Код для открытия бокса №{order.box.id}'
+            body_html = f'''
+            <html>
+                <body>
+                    <p>Код ключа: {order.access_code}</p>
+                    <img src="qr-code" />
+                </body>
+            </html>
+            '''
+
+            from_email = settings.EMAIL_HOST_USER
+            to_email = current_user.username
+
+            msg = EmailMultiAlternatives(
+                subject,
+                body_html,
+                from_email=from_email,
+                to=[to_email]
+            )
+
+            msg.mixed_subtype = 'related'
+            msg.attach_alternative(body_html, "text/html")
+            image = unquote(order.access_qr.name)
+            file_path = reduce(os.path.join, ['media', image])
+            with open(file_path, 'br') as f:
+                img = MIMEImage(f.read())
+            msg.attach(img)
+            msg.send()
+
+            return redirect(my_rent)
+        else:
+            if request.POST['PASSWORD_EDIT'] != 'new password':
+                current_user.set_password(request.POST['PASSWORD_EDIT'])
+                current_user.save()
+            if current_user.client.phone != request.POST['PHONE_EDIT']:
+                parsed_phonenumber = phonenumbers.parse(request.POST['PHONE_EDIT'], 'RU')
+                if phonenumbers.is_valid_number(parsed_phonenumber):
+                    formatted_phonenumber = phonenumbers.format_number(
+                        parsed_phonenumber,
+                        phonenumbers.PhoneNumberFormat.E164
+                    )
+                current_user.client.phone = formatted_phonenumber
+                current_user.client.save()
 
     return render(request, 'my-rent.html')
